4a4d3f68683c254d2cb3d6eb3ca318450b1d4d06
Praisecodes <cmancoder@gmail.com>
2022-03-04 15:32:58 +0100

    Update JavaScript

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,28 @@ const DisplayJoinBlock = () => {
 const CloseJoinBlock = () => {
     document.querySelector(".join-drop").style.display = "none";
 }
+
+const images = [
+    "./images/Official Opening.jpg",
+    "./images/Students.jpg",
+    "./images/SSTAC-building.jpg",
+    "./images/site.jpg",
+    "./images/Chemistry.jpg"
+];
+
+let chk = 0;
+setInterval(() => {
+    setTimeout(() => {
+        document.querySelector(".image").classList.add("fadingOut");
+        setTimeout(() => {
+            chk++;
+            if(chk >= images.length){
+                chk=0;
+            }
+            document.querySelector(".image").src = images[chk];
+            setTimeout(()=>{
+                document.querySelector(".image").classList.add("fadingOut");
+            },0);
+        }, 1000);
+    }, 1000);
+}, 4000);
