7ec3224dd18793c09743b59c2719cef56d0819bc
Praisecodes <cmancoder@gmail.com>
2022-03-04 14:20:07 +0100

    Update Header

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+const DisplayAboutBlock = () => {
+    document.querySelector(".about-drop").style.display = "block";
+}
+
+const CloseBlock = () => {
+    document.querySelector(".about-drop").style.display = "none";
+}
+
+const DisplayCuBlock = () => {
+    document.querySelector(".curriculum-drop").style.display = "block";
+}
+
+const CloseCuBlock = () => {
+    document.querySelector(".curriculum-drop").style.display = "none";
+}
+
+const DisplayEventsBlock = () => {
+    document.querySelector(".events-drop").style.display = "block";
+}
+
+const CloseEventsBlock = () => {
+    document.querySelector(".events-drop").style.display = "none";
+}
+
+const DisplayJoinBlock = () => {
+    document.querySelector(".join-drop").style.display = "block";
+}
+
+const CloseJoinBlock = () => {
+    document.querySelector(".join-drop").style.display = "none";
+}
