5a806614c596624462bf19e99ac6178205d657a3
Praisecodes <cmancoder@gmail.com>
2022-03-05 21:44:02 +0100

    Updates on page

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+window.onscroll = () => {
+    if(this.scrollY > 100){
+        document.querySelector(".up").style.display = "block";
+    }
+    else{
+        document.querySelector(".up").style.display = "none";
+    }
+}
+
+document.querySelector(".up").addEventListener('click', () => {
+    document.querySelector(".contact-top").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.querySelector(".mobile-btn").addEventListener('click', () => {
+    document.querySelector(".mobile-nav-wrap").style.display = "block";
+    document.body.style.overflow = "hidden";
+});
